5769b55b09a20e41e7fc5a974fb397c5d1834612
aaronbugden210 <aaronbugden@gmail.com>
2019-07-24 12:29:22 -0400

    Updates for better functionality

--- assets/js/mains.js
+++ assets/js/mains.js
@@ -1,4 +1,6 @@
-$("li").click(function()
+//the entire 'ul' needs a listener for this project,
+//at least for new todos that are dynamically created
+$("ul").on("click", "li", function()
 {
     //Code can change the style of the elements with jQuery
 
@@ -19,4 +21,31 @@ $("li").click(function()
 
     //but can also be done with CSS easier
     $(this).toggleClass("completed");
+
+});
+
+//click on 'X' to delete To-Do
+$("ul").on("click", "span", function(event) 
+{
+    $(this).parent().fadeOut(500, function() 
+    {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//adds new todo to list
+$("input[type='text']").keypress(function(event)
+{
+    if(event.which === 13)
+    {
+        var newToDo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + newToDo + "</li>");
+    }
+});
+
+$(".fa-plus").click(function()
+{
+    $("input[type='text']").fadeToggle();
 });
